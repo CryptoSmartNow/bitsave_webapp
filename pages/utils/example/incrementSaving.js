fc821a9edacf262fda1e0fe9445dbdba48b4cfe0
xpan <xpanvictor@gmail.com>
2023-07-18 16:06:36 +0100

    feat: all integrations implementations with contract done

--- pages/utils/example/incrementSaving.js
+++ pages/utils/example/incrementSaving.js
@@ -24,7 +24,7 @@ export async function increaseSaving(
         )
 
         console.log(savingsIncResult)
-        return savingsIncResult
+        return savingsIncResult;
     } catch (error) {
         console.log(error)
     }
