585b4c6165b75748032f82611f7630d9915934c9
xpan <xpanvictor@gmail.com>
2023-07-20 01:26:34 +0100

    fix: uses new abi method

--- pages/utils/approveAmount.js
+++ pages/utils/approveAmount.js
@@ -11,7 +11,7 @@ export default async function approveAmount(
         const PaymentToken = makePaymentTokenInstance(signer);
 
         const parsedAmount = ethers.utils.parseUnits(
-            amount
+            amount?.toString()
         )
 
         await PaymentToken.approve(
